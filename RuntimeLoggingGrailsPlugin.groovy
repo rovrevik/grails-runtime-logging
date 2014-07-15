a0acc16fa04a5a6f4648c4906ff9dce8a44a34c9
Ryan Ovrevik <rovrevik@ovrevik.com>
2014-07-15 14:14:03 -0400

    add snapshot version sufix

--- RuntimeLoggingGrailsPlugin.groovy
+++ RuntimeLoggingGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class RuntimeLoggingGrailsPlugin {
     // the plugin version
-    def version = "0.4.1"
+    def version = "0.4.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
